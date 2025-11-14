9b4f8789d6a77f7b48c62933973487dd07ad8fe9
ikeruco29 <ikeruco29@gmail.com>
2025-11-14 13:55:15 +0100

    Added an option to reset all manual interfaces to DHCP

--- setip.ps1
+++ setip.ps1
@@ -3,8 +3,11 @@ param (
     [Parameter(Mandatory = $true)]
     [string]$mode,
     
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false)]
     [string]$ipaddr,
+
+    [Parameter(Mandatory = $false)]
+    [switch]$all,
     
     [Parameter(Mandatory = $false)]
     [int]$interfaceIdx = 4
@@ -12,16 +15,30 @@ param (
 
     
 if($null -eq $mode){
-    Write-Output "Needs to specify mode"
-    Write-Output "Example:"
-    Write-Output "      setip.ps1 set/reset"
-} else {
+    throw "Needs to specify mode`nExample:`n    setip.ps1 set"
+} elseif (-not $ipaddr -and -not $all ) {
+    throw "Target IP address needs to be specified. `nUse -ipaddr to specify it, or use -all with the mode 'reset' to reset all static IP configured in either Wi-Fi or Ethernet network interfaces."
+} elseif ($mode -eq "set" -and $all) {
+    throw "Cannot use -all with 'set' mode."
+}
+else {
     if($mode -eq "set"){
+        Set-NetIPInterface -InterfaceIndex $interfaceIdx -Dhcp Disabled
         New-NetIPAddress -InterfaceIndex $interfaceIdx -IPAddress $ipaddr
         Set-NetIPAddress -InterfaceIndex $interfaceIdx -IPAddress $ipaddr -PrefixLength 24 
     } else {
-        Remove-NetIPAddress -InterfaceIndex $interfaceIdx -IPAddress $ipaddr -Confirm:$false
-        Set-NetIPInterface -InterfaceIndex $interfaceIdx -Dhcp Enabled
+        if($all){   # IF -all has been setted
+            # Get ethernet and wifi interfaces that are not in DHCP
+            $networkInterfaces = Get-NetIPAddress | Where-Object { $_.PrefixOrigin -like "manual" -and ($_.InterfaceAlias -eq "Ethernet" -or $_.InterfaceAlias -eq "Wi-Fi") }
+            
+            foreach ($networkInterface in $networkInterfaces) { # For each one, reset the IPAddress and enable de DHCP
+                Remove-NetIPAddress -InterfaceIndex $networkInterface.InterfaceIndex -IPAddress $networkInterface.IPAddress -Confirm:$false
+                Set-NetIPInterface -InterfaceIndex $networkInterface.InterfaceIndex -Dhcp Enabled
+            }
+        } else {
+            Remove-NetIPAddress -InterfaceIndex $interfaceIdx -IPAddress $ipaddr -Confirm:$false
+            Set-NetIPInterface -InterfaceIndex $interfaceIdx -Dhcp Enabled
+        }
     }
     Write-Output "Done..."
     Get-NetIPAddress | Where-Object { $_.InterfaceIndex -eq $interfaceIdx } | Format-Table
